aec4fab162d5ec025edff5eeb4dd075001f3b90a
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-03-02 15:13:42 +0100

    Prepinanie medzi komponentami

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { SiGoogleanalytics } from "react-icons/si";
-import Details from "./Details";
 
 const formatTime = (ms) => {
   let seconds = Math.floor(ms / 1000);
@@ -16,10 +15,9 @@ const formatTime = (ms) => {
   return formattedTime;
 };
 
-function App() {
+function Details({ onBack }) {
   const [times, setTimes] = useState({});
   const [isTracking, setIsTracking] = useState(true);
-  const [view, setView] = useState("home");
 
   useEffect(() => {
     const fetchTimes = () => {
@@ -51,35 +49,31 @@ function App() {
   };
 
   return (
-    <div>
-      {view === "home" ? (
-        <div className="w-[500px] flex flex-col justify-center items-center bg-[#212329] text-white">
-          <h1 className="text-3xl my-3">FocusTab</h1>
-          <div className="flex space-x-3">
-            <button 
-              onClick={toggleTracking} 
-              className={`px-4 py-2 mb-3 rounded ${isTracking ? 'bg-red-500 hover:bg-red-600' : 'bg-green-500 hover:bg-green-600'}`}
-            >
-              {isTracking ? 'Vypnúť meranie' : 'Zapnúť meranie'}
-            </button>
-          </div>
-          <ul className="w-[90%]">
-            {Object.entries(times).map(([domain, time]) => (
-              <li key={domain} className="w-full flex justify-between bg-[#3f3f3f] p-2 text-white mb-2 rounded-sm">
-                <span>{domain}</span>
-                <span>{formatTime(time)}</span>
-              </li>
-            ))}
-          </ul>
-          <div className='absolute top-5 right-5' onClick={() => setView("details")}>
-            <SiGoogleanalytics color='green' size='20' />
-          </div>
-        </div>
-      ) : (
-        <Details onBack={() => setView("home")} />
-      )}
+    <div className="w-[500px] flex flex-col justify-center items-center bg-[#212329] text-white">
+      <h1 className="text-3xl my-3">FocusTabbb</h1>
+
+      <div className="flex space-x-3">
+        <button 
+          onClick={toggleTracking} 
+          className={`px-4 py-2 mb-3 rounded ${isTracking ? 'bg-red-500 hover:bg-red-600' : 'bg-green-500 hover:bg-green-600'}`}
+        >
+          {isTracking ? 'Vypnúť meranie' : 'Zapnúť meranie'}
+        </button>
+      </div>
+
+      <ul className="w-[90%]">
+        {Object.entries(times).map(([domain, time]) => (
+          <li key={domain} className="w-full flex justify-between bg-[#3f3f3f] p-2 text-white mb-2 rounded-sm">
+            <span>{domain}</span>
+            <span>{formatTime(time)}</span>
+          </li>
+        ))}
+      </ul>
+      <div className='absolute top-5 right-5' onClick={onBack}>
+        <SiGoogleanalytics color='green' size='20' />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Details;
